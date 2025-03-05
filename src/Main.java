cac41cbe6b420804012ad1103df88c71eb10c69c
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-05 14:28:27 -0300

    Add new output on Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I love my girlfriend!!");
+        System.out.println("I really love my girlfriend!!");
     }
 }
